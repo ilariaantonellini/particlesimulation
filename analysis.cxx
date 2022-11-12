eee6e07230234b7f2553fe88e5d599b5e6631fa1
Ilaria Antonellini <antonellinilaria@gmail.com>
2022-11-12 16:37:25 +0100

    Fixed bug

--- analysis.cxx
+++ analysis.cxx
@@ -47,20 +47,6 @@ void analysis() {
   histograms.push_back(h11);
   histograms.push_back(h12);
 
-  for (int i = 0; i != 12; ++i) {
-    // Printig entries for each histogram
-    std::cout << i + 1 << " Entries: " << histograms[i]->GetEntries() << '\n';
-  }
-
-  // Analysis of generated particles
-  std::cout << "Check proportions of types of particle generated: " << '\n';
-  for (int i = 1; i != 8; ++i) {
-    std::cout << "Bin " << i << " content: " << h1->GetBinContent(i) << "+/-"
-              << h1->GetBinError(i) << '\n';
-    std::cout << "Bin " << i << " pencentage of particles: "
-              << (h1->GetBinContent(i) / h1->GetEntries()) << '\n';
-  }
-
   // Fitting angles distribution
   double const cazimutal = 1 / (2 * M_PI);
   double const cpolar = 1 / (M_PI);
@@ -71,30 +57,19 @@ void analysis() {
   h2->Fit(fazimutal);
   h3->Fit(fpolar);
 
-  std::cout << "h2 fit parameters, chi square and probability of fit: "
-            << fazimutal->GetParameter(0) << " "
-            << fazimutal->GetChisquare() / fazimutal->GetNDF() << " "
-            << fazimutal->GetProb() << '\n';
-  std::cout << "h3 fit parameters, chi square and probability of fit: "
-            << fpolar->GetParameter(0) << " "
-            << fpolar->GetChisquare() / fpolar->GetNDF() << " "
-            << fpolar->GetProb() << '\n';
-
   // Fitting magnitude of p
   TF1 *fp = new TF1("fp", "expo", 0, 10);
+  fp->SetParameter(1, -1);
   h4->Fit(fp);
-  std::cout << "h4 fit parameters, chi square and probability of fit: "
-            << fp->GetParameter(0) << " " << fp->GetParameter(1) << " "
-            << fp->GetChisquare() / fp->GetNDF() << " " << fp->GetProb()
-            << '\n';
+
   // Analysis of invariant mass
   TH1F *h13 = new TH1F(*h8);
-  h13->Add(h8, h9, 1, -1);
-
   TH1F *h14 = new TH1F(*h10);
+
+  h13->Add(h8, h9, 1, -1);
   h14->Add(h10, h11, 1, -1);
   // al posto di fare le entries usare il metodo integral che da' le entries
-  // corret
+  // correte
   double const kmass = 0.89166;
   double const kwidth = 0.050;
 
@@ -108,45 +83,97 @@ void analysis() {
   h13->Fit(finvmass1);
   h14->Fit(finvmass2);
 
-  std::cout << "h13 fit parameters, chi square and probability of fit: "
-            << finvmass1->GetParameter(0) << " " << finvmass1->GetParameter(1)
-            << " " << finvmass1->GetParameter(2) << " "
-            << finvmass1->GetChisquare() / finvmass1->GetNDF() << " "
-            << finvmass1->GetProb() << '\n';
-
-  std::cout << "h14 fit parameters, chi square and probability of fit: "
-            << finvmass2->GetParameter(0) << " " << finvmass2->GetParameter(1)
-            << " " << finvmass2->GetParameter(2) << " "
-            << finvmass2->GetChisquare() / finvmass2->GetNDF() << " "
-            << finvmass2->GetProb() << '\n';
-
   // Fitting
 
   histograms.push_back(h13);
   histograms.push_back(h14);
 
-  // Drawing statistical part
+  // Drawing statistical part on canvas
 
-  //sta dando errori di segmentation
-  TCanvas *c = new TCanvas("c", "Histograms", 200, 10, 600, 400);
-  c->Divide(5, 3);
+  TCanvas *c1 = new TCanvas("c1", "Histograms", 200, 10, 600, 400);
+  c1->Divide(5, 3);
 
   for (int i = 0; i != 14; ++i) {
-    c->cd(i + 1);
+    c1->cd(i + 1);
     histograms[i]->DrawCopy("H");
     histograms[i]->DrawCopy("E, SAME");
-    if (i == 1) {
-      fazimutal->Draw("APE");
-    } else if (i == 2) {
-      fpolar->Draw("APE");
-    } else if (i == 3) {
-      fp->Draw("APE");
-    } else if (i == 13) {
-      finvmass1->Draw("APE");
-    } else if (i == 14) {
-      finvmass2->Draw("APE");
-    }
   }
+
+  // Saving canvas data
+
+  c1->Print("analysis.pdf");
+  c1->Print("analysis.C");
+  c1->Print("analysis.root");
+
+  // Printing on screen
+
+  for (int i = 0; i != 12; ++i) {
+    // Printig entries for each histogram
+    std::cout << i + 1 << " Entries: " << histograms[i]->GetEntries() << '\n';
+  }
+
+  // Analysis of generated particles
+  std::cout << '\n' << "Data Analysis: " << '\n' << '\n';
+
+  std::cout << "Check proportions of types of particle generated: " << '\n';
+  for (int i = 1; i != 8; ++i) {
+    std::cout << "Bin " << i << " content: " << h1->GetBinContent(i) << " "
+              << "+/-"
+              << " " << h1->GetBinError(i) << '\n';
+    std::cout << "Bin " << i << " pencentage of particles: "
+              << (h1->GetBinContent(i) / h1->GetEntries()) << '\n';
+  }
+
+  std::cout << '\n' << "Result of fits: " << '\n' << '\n';
+
+  std::cout << "h2 fit parameters, chi square and probability of fit: " << '\n'
+            << fazimutal->GetParameter(0) << " "
+            << "+/-"
+            << " " << fazimutal->GetParError(0) << '\n'
+            << fazimutal->GetChisquare() / fazimutal->GetNDF() << '\n'
+            << fazimutal->GetProb() << '\n';
+  std::cout << "h3 fit parameters, chi square and probability of fit: " << '\n'
+            << fpolar->GetParameter(0) << " "
+            << "+/-"
+            << " " << fpolar->GetParError(0) << '\n'
+            << fpolar->GetChisquare() / fpolar->GetNDF() << '\n'
+            << fpolar->GetProb() << '\n';
+
+  std::cout << "h4 fit parameters, chi square and probability of fit: " << '\n'
+            << fp->GetParameter(0) << " "
+            << "+/-"
+            << " " << fp->GetParError(0) << '\n'
+            << fp->GetParameter(1) << " "
+            << "+/-"
+            << " " << fp->GetParError(1) << '\n'
+            << fp->GetChisquare() / fp->GetNDF() << '\n'
+            << fp->GetProb() << '\n';
+
+  std::cout << "h13 fit parameters, chi square and probability of fit: " << '\n'
+            << finvmass1->GetParameter(0) << " "
+            << "+/-"
+            << " " << finvmass1->GetParError(0) << '\n'
+            << finvmass1->GetParameter(1) << " "
+            << "+/-"
+            << " " << finvmass1->GetParError(1) << '\n'
+            << finvmass1->GetParameter(2) << " "
+            << "+/-"
+            << " " << finvmass1->GetParError(2) << '\n'
+            << finvmass1->GetChisquare() / finvmass1->GetNDF() << '\n'
+            << finvmass1->GetProb() << '\n';
+
+  std::cout << "h14 fit parameters, chi square and probability of fit: " << '\n'
+            << finvmass2->GetParameter(0) << " "
+            << "+/-"
+            << " " << finvmass2->GetParError(0) << '\n'
+            << finvmass2->GetParameter(1) << " "
+            << "+/-"
+            << " " << finvmass2->GetParError(1) << '\n'
+            << finvmass2->GetParameter(2) << " "
+            << "+/-"
+            << " " << finvmass2->GetParError(2) << '\n'
+            << finvmass2->GetChisquare() / finvmass2->GetNDF() << '\n'
+            << finvmass2->GetProb() << '\n';
 
   file->Close();
 }
